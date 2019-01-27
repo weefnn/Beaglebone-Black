5d2694e5c1ecfea05ffb10f519a0279011cb7174
phil wei <weefnn@qq.com>
2019-01-27 13:29:36 +0000

    delete unused file

--- python/gpio_int_block.py
+++
@@ -1,9 +0,0 @@
-import Adafruit_BBIO.GPIO as GPIO
-from time import sleep
-
-GPIO.setup("P8_14", GPIO.IN)
-
-while True:
-    GPIO.wait_for_edge("P8_14", GPIO.RISING)
-    print "we get a rising edge!"
-    sleep(1)
--- python/gpio_output.py
+++
@@ -1,8 +0,0 @@
-import Adafruit_BBIO.GPIO as GPIO
-
-GPIO.setup("P8_10", GPIO.OUT)
-GPIO.output("P8_10", GPIO.HIGH)
-#You can also refer to the pin names
-#GPIO.setup("GPIO0_26", GPIO.OUT)
-#GPIO.output("GPIO_26", GPIO.HIGH)
-GPIO.cleanup()
